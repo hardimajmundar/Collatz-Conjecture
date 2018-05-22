66f98431efeae3398cc0c763e514a7b572a6bfa6
Hardi Shah <hardimajmundar638@gmail.com>
2018-05-22 11:21:39 -0500

    initial commit

--- Application1/Application1/CollatzConjecture.cs
+++ Application1/Application1/CollatzConjecture.cs
@@ -6,10 +6,56 @@ using System.Threading.Tasks;
 
 namespace Application1
 {
+    public class evenoddfunction
+    {
+        public int evenoddfun(int positiveno)
+        {
+            if (positiveno % 2 == 0)
+            {
+                positiveno = positiveno / 2;
+            }
+            else
+            {
+                positiveno = (3 * positiveno) + 1;
+            }
+            return positiveno;
+        }
+    }
+
+    public class mainfuncation
+    {
+        int count = 0;
+        public int positiveno;
+        public int mainfun()
+        {
+            Console.WriteLine("Enter Positive Number:");
+            positiveno = int.Parse(Console.ReadLine());
+            if (positiveno < 0)
+            {
+                Console.WriteLine("Number is nagative.");
+                return 0;
+            }
+            else
+            {
+                evenoddfunction evod = new evenoddfunction();
+                while (positiveno != 1)
+                {
+                    positiveno = evod.evenoddfun(positiveno);
+                    count++;
+                }
+                Console.WriteLine("Number of steps:" + count);
+            }
+            return count;
+        }
+    }
+
     class CollatzConjecture
     {
         static void Main(string[] args)
         {
+            mainfuncation fun = new mainfuncation();
+            fun.mainfun();
+            Console.ReadKey();
         }
     }
 }
